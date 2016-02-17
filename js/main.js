a32af0dac9edd271a147138ab4c6938573a962ce
bhare1987 <brandonlhare@gmail.com>
2016-02-17 15:12:19 -0500

    wrote parse date function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+  var joinedOn = parseUTCDate(username.created_at)
   $("li > img.avatar").attr("src", username.avatar_url);
   $("div.avatar > img").attr("src", username.avatar_url);
+  $(".name").text(username.name);
+  $(".userName").text(username.login);
+  $(".info li:nth-child(1)").text(username.company);
+  $(".info li:nth-child(2)").text(username.location);
+  $(".info li:nth-child(3)").text("Joined on " + joinedOn.month + " " + joinedOn.day + ", " + joinedOn.year);
+
 
 
 
@@ -24,3 +31,15 @@ $(document).ready(function(){
 
 
 });
+
+function parseUTCDate(date) {
+  var dateObj = {
+        fullDate: moment.utc(date).format("dddd, MMMM Do YYYY, h:mm:ss a"),
+        year: moment.utc(date).format("YYYY"),
+        month: moment.utc(date).format("MMM"),
+        day: moment.utc(date).format("DD"),
+        weekday: moment.utc(date).format("ddd"),
+        time: moment.utc(date).format("h:mm:ss a")
+      }
+  return dateObj;
+}
